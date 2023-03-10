a0ce10896ab98fa26fab1bf0e0299beb4e8768d5
MayankSharmaKellogg <mayank.sharma2@kellogg.northwestern.edu>
2023-03-10 00:55:30 +0000

    finally___done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,17 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Hello."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Nope."
       redirect_to "/login"
     end
   end
 
   def destroy
     session["user_id"] = nil
-    flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
 end
